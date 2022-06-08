b9326f288b793c5a7f58b39ecc3ba29b4ebe6f40
Kenyansa <kenyansajustine@gmail.com>
2022-06-08 10:51:24 +0300

    first commit

---
+++ ruby.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+  words = sentence.split
+  reversed_words = []
+
+  words.each do |word|
+    reversed_words << word.reverse
+  end
+
+  reversed_words.join(" ")
+end
+
+reverse_each_word("I love you!")
