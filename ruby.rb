a942b1700b1387ee657c00ea3fc4f91ee457ac42
Kenyansa <kenyansajustine@gmail.com>
2022-06-08 11:11:46 +0300

    use map, join methods to reverse word in a sentence

--- ruby.rb
+++ ruby.rb
@@ -12,3 +12,11 @@ def reverse_each_word(sentence)
 end
 
 reverse_each_word("I love you!")
+
+#usign map method
+def reverse_each_word(sentence)
+  words = sentence.split
+
+  reversed_words = words.map(&:reverse).join(" ")
+end
+reverse_each_word("TOday is a great day for us to do some programming!")
